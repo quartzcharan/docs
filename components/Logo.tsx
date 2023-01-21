7b08a673513472fb41c4c403f8f48c71c78b6c72
Charan Velusamy <charanquartzzz@gmail.com>
2023-01-21 22:21:29 +0530

    sa

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,9 +2,10 @@ import styles from "./Logo.module.css";
 
 const Logo = ({ className = "", ...props }) => (
   <div className={styles.logo}>
-    <img src='https://assets-charanquartz.netlify.app/a/w/docs.assets/cq.svg' alt='next' />
+    <img src='https://home.charanquartz.vercel.app/c.gif' width='50' height='50'alt='next' />
+    
     <strong>Charanquartz's Documentation</strong>
-    <img src='https://home.charanquartz.vercel.app/c.gif' alt='next' />
+    <img src='#https://assets-charanquartz.netlify.app/a/w/docs.assets/cq.svg' alt='next' />
     </div>
 );
 
