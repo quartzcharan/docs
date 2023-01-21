c449e4807e1c8c92b95a36fc7ea0fef70b27d238
Charan Velusamy <charanquartzzz@gmail.com>
2023-01-22 00:47:45 +0530

    saved

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   latex: true,
-  defaultShowCopyCode: true,
+  defaultShowCopyCode: false,
 });
 
 module.exports = withNextra();
