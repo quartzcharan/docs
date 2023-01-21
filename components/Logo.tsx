1adb58f7b0325c44e74b3f79acd7d684e5c25d07
Charan Velusamy <charanquartzzz@gmail.com>
2023-01-21 19:01:20 +0530

    saved

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@ import styles from "./Logo.module.css";
 
 const Logo = ({ className = "", ...props }) => (
   <div className={styles.logo}>
-    <img src='https://s4.aconvert.com/convert/p3r68-cdx67/aqvcx-g5cm9.svg' alt='next' />
+    <img src='./s.svg' alt='next' />
 
     <strong>Nakafa</strong>
   </div>
