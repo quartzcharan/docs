f646e43015e74405044a7fc3dd5dc7ad153a21c0
Charan Velusamy <charanquartzzz@gmail.com>
2023-01-21 19:52:00 +0530

    saved

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,9 +2,9 @@ import styles from "./Logo.module.css";
 
 const Logo = ({ className = "", ...props }) => (
   <div className={styles.logo}>
-    <img src='https://assets.charanquartz.vercel.app/a/w/docs.assets/charanquartzsdocumentation.svg'  height = "50" width = "200"alt='next' />
+    <img src='https://assets.charanquartz.vercel.app/a/w/docs.assets/cq.svg'  alt='next' />
 
-    <strong>Nakafa</strong>
+    <strong>Charanquartz's Documentation</strong>
   </div>
 );
 
