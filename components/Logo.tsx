d84b52a499a40a351dfd8ee0ba70a28bd7ea578f
Charan Velusamy <charanquartzzz@gmail.com>
2023-01-26 20:54:49 +0530

    saveed

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import styles from "./Logo.module.css";
 const Logo = ({ className = "", ...props }) => (
   <div className={styles.logo}>
     <img src='https://assets-charanquartz.netlify.app/a/w/docs.assets/cq.svg' alt='next' />
-    <strong>CQ's Documentation </strong>
+    <strong>CQ's Docs </strong>
    
     </div>
 
